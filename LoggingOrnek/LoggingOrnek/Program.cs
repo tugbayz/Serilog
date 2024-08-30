27822e9fe8039ed9ff76639a8148fcfcc9982d5a
tugbayz <tugbaayaaz@hotmail.com>
2024-08-30 21:54:24 +0300

    Serilog
    Konsol için küçük bir serilog kullanım örneği

---
+++ LoggingOrnek/LoggingOrnek/Program.cs
@@ -0,0 +1,25 @@
+﻿//, rollingInterval: RollingInterval.Day
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+          .MinimumLevel.Debug() 
+          .WriteTo.Console()
+          .WriteTo.File("logs/myapp.txt")
+          .CreateLogger();
+
+Log.Information("Uygulama başlatıldı");
+Log.Warning("Bu bir uyarı mesajıdır");
+Log.Error("Bu bir hata mesajıdır");
+
+try
+{
+    var result = 10 / int.Parse("0"); // bilerek hata
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Beklenmeyen bir hata oluştu!");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
